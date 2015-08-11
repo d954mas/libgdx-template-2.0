ccc13c1bddfdfa9784b64e6f1a99a6d6dd4b1d19
d954mas <d954mas@gmail.com>
2015-08-11 15:10:30 +0300

    retrolambda fixes gitignore not work

--- android/src/com/d954mas/game/android/AndroidLauncher.java
+++ android/src/com/d954mas/game/android/AndroidLauncher.java
@@ -1,23 +1,32 @@
 package com.d954mas.game.android;
 
 import android.os.Bundle;
+import android.util.Log;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.badlogic.gdx.utils.OrderedMap;
 import com.d954mas.game.EsqGame;
 import com.d954mas.game.services.Service;
 import com.d954mas.game.services.iface.NativeApiService;
+
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        runOnUiThread(() -> {
+            Log.d("Main Thread", "Lambda works!");
+        });
+
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         OrderedMap<Class<? extends Service>,Service> services=new OrderedMap<Class<? extends Service>,Service>();
         services.put(NativeApiService.class, new NativeApiService() {
             @Override
-            public void dispose() {}
+            public void dispose() {
+            }
+
             @Override
-            public void init() {}
+            public void init() {
+            }
         });
 		initialize(new EsqGame(services), config);
 	}
--- core/src/com/d954mas/game/EsqGame.java
+++ core/src/com/d954mas/game/EsqGame.java
@@ -44,6 +44,8 @@ public class EsqGame extends Game {
         Screens.instance.init(this);
         Gdx.app.setLogLevel(Application.LOG_DEBUG);
         Screens.instance.setScreen(LogoScreen.class);
+       // List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+      //  numbers.forEach(value -> System.out.println(value));
     }
 
     @Override
